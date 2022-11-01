4143df1b208191d95b1bc8d42a19f1ea2a42c8fc
Funjoya <ilanslama545@gmail.com>
2022-11-01 20:52:07 +0200

    Login for elad

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,9 @@
 import { View, Text,  StyleSheet, Image} from 'react-native'
-import React from 'react'
+import React, {useState} from 'react'
 
+
+//import {MotiView, useAnimationState} from "moti";
+//import { Shadow } from "react-native-shadow-2";
 // import { 
 //   TextButton
 //  } from "../components";
@@ -9,11 +12,37 @@ import React from 'react'
   } from "../constants";
 
 
+
 const Login = () => {
+  const [mode, setMode] = React.useState("signIn")
+  const animationState = useState({
+    signIn: {
+      height: "50%"
+    },
+    signUp:{
+      height: "50%"
+    }
+  })
+
+  function renderSignIn(){
+    
+  }
+  function renderSignUp(){
+    
+  }
+  function renderAuthComtainer(){
+    if(mode == "signIn"){
+      return renderSignIn()
+    }
+    else{
+      return renderSignUp()
+    }
+  }
   return (
     <View style={styles.container}>
       <Image source={images.background} style={styles.backgroundStyle}/>
 
+      {/*  logo  */}
       <Image
         source={images.logo}
         style={{
@@ -23,6 +52,12 @@ const Login = () => {
           height: 22
         }}/>
 
+        {/*  Auth container  */}
+        <View>
+          {renderAuthComtainer()}
+
+
+        </View>
     </View>
   )
 }
@@ -35,7 +70,17 @@ const styles = StyleSheet.create({
       
   },
   backgroundStyle:{
-    resizeMode:'cover'
+    flex: 1,
+    resizeMode:'stretch',
+    
+  },
+  authContainer: {
+    flex: 1,
+    width: "100%",
+    paddingHorizontal: "10%",
+    paddingVertical: "10%",
+    borderRadius: "5%"
   }
 })
+
 export default Login
